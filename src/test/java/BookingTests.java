56d891339b2ae1f9bc88c5f8854f3b0c62c7ba1b
Estela Magalhaes <estelinha.mag@hotmail.com>
2022-11-08 11:39:06 -0300

    teste getAllBookingById_returnOk

--- src/test/java/BookingTests.java
+++ src/test/java/BookingTests.java
@@ -3,11 +3,18 @@ import Entities.BookingDates;
 import Entities.User;
 import com.github.javafaker.Faker;
 import io.restassured.RestAssured;
+import io.restassured.config.LogConfig;
+import io.restassured.config.RestAssuredConfig;
 import io.restassured.filter.log.ErrorLoggingFilter;
 import io.restassured.filter.log.RequestLoggingFilter;
 import io.restassured.filter.log.ResponseLoggingFilter;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class BookingTests {
     public static Faker faker;
@@ -36,4 +43,20 @@ public class BookingTests {
         RestAssured.filters(new RequestLoggingFilter(), new ResponseLoggingFilter(), new ErrorLoggingFilter());
     }
 
+    @BeforeEach
+    void setRequest(){
+        request = request.given().config(RestAssuredConfig.config().logConfig(LogConfig.logConfig().enableLoggingOfRequestAndResponseIfValidationFails()))
+                .contentType(ContentType.JSON)
+                .auth().basic("admin","password123");
+    }
+    @Test
+    public void getAllBookingById_returnOk(){
+        Response response = request
+                                .when()
+                                .get()
+                                .then()
+                                .extract().response();
+        Assertions.assertNotNull(response);
+        Assertions.assertEquals(200, response.statusCode());
+    }
 }
